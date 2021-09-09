96705b33e5a595695c0d18147b7271a822e84d94
CrveniFlash <redflashms@gmail.com>
2021-09-10 00:43:52 +0200

    finished

--- public/script.js
+++ public/script.js
@@ -4,9 +4,13 @@ const msgSendBtn = document.getElementById('send-btn')
 const msgDisplay = document.getElementById('msg-display')
 const msgCounter = document.getElementById('msg-counter')
 const showHideText = document.getElementById('show-hide-text')
+const button = document.getElementById('buttonmodal')
+const closebutton = document.getElementById('closebutton')
+const modal = document.getElementById('modal')
 
 
-    let msgCount = 0;
+
+let msgCount = 0;
 
 
 msgSendBtn.addEventListener('click', () => {
@@ -15,12 +19,65 @@ msgSendBtn.addEventListener('click', () => {
     msgDisplay.innerText = msgInput.value
     msgCounter.innerText = msgCount
     
+    //message logs
+
+    let msgLog = document.createElement('p');
+    msgLog.classList.add('border-t-2')
+    msgLog.innerHTML = msgInput.value
+
+
+    document.getElementById('message-logs').appendChild(msgLog)
+
+    const date = new Date();
+    
+    const local = date.toLocaleTimeString('en-US');
+    let par = document.createElement('p')
+    par.innerHTML = local
+    document.getElementById('message-logs').appendChild(par)
+
+    
 })
 
-showHideText.addEventListener('click', function(event) {
-    if(this.checked) {
+showHideText.addEventListener('click', function (event) {
+    if (this.checked) {
         msgInput.type = 'password';
     } else {
         msgInput.type = 'text';
     }
 })
+
+
+
+button.addEventListener('click',()=>modal.classList.add('scale-100'))
+closebutton.addEventListener('click',()=>modal.classList.remove('scale-100'))
+
+// dynamic clock
+
+function realClock() {
+    const time = new Date()
+    let h = time.getHours()
+    let m = time.getMinutes()
+    let s = time.getSeconds()
+    m = checkTime(m)
+    s = checkTime(s)
+    document.getElementById('dynamic-clock').innerHTML = h + ':' + m + ':' + s
+    setTimeout(realClock, 1000);
+
+    function checkTime(i) {
+        if(i < 10) {i = '0' + i}
+        return i;
+    }
+
+    // let newTimeLog = `${h}:${m}:${s}`
+    // let timeLog = newTimeLog
+    // let timeSpan = document.createElement('span')
+    // timeSpan.appendChild()
+    // timeSpan.classList.add('ml-10')
+
+    // document.getElementById('message-logs').appendChild(timeSpan)
+
+}
+
+function messageLogs () {
+    
+}
